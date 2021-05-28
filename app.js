ac607bc20fce6c88443c085e348fa8e596332439
Temi-Tegbe <66542303+Temi-Tegbe@users.noreply.github.com>
2021-05-28 17:04:45 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+let amount = window.prompt("Enter amount here:");
+
+if (amount = "") {
+    alert("no input recorded");
+} else if (amount = isNaN) {
+    alert("please input a number");
+} else if (amount > 0 && amount <= 5000) {
+    alert("Commission is ₦10");
+} else if (amount > 5000 && amount < 50000) {
+    alert("Commission is ₦25");
+} else {
+    alert("Commmission is ₦50");
+}
